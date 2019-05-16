f6c3186f2486202555b2c372214f86c301c6d5af
saraen <saraestravis@gmail.com>
2019-05-16 22:10:36 +0200

    Create MatrixAnalysisResults.m

---
+++ MatrixAnalysisResults.m
@@ -0,0 +1,19 @@
+classdef MatrixAnalysisResults < handle
+   
+    %MatrixAnalysisResults This class contains de results of the analysis
+    % of one population with the same matrix type (FA or SC)
+    
+    properties
+        %Strength
+        strengthMean = 0
+        strengthSd = 0
+        strengthTtest = 0
+        strengthPvalue = 0  
+        
+    end
+    
+    methods
+    end
+    
+end
+
