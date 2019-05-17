06f34a7710a2f6e417a8ba3b3de37211eb38321e
saraen <saraestravis@gmail.com>
2019-05-17 22:39:50 +0200

    Update Cohort.m with the functions that show the strength analysis

--- Cohort.m
+++ Cohort.m
@@ -85,15 +85,128 @@ classdef Cohort < handle
         function showAnalysisResults(obj)
             
             disp('STRENGHT - FA MATRIX');
-            disp(['MS: ', num2str(obj.FApatientsResults.strengthMean), ' (', num2str(obj.FApatientsResults.strengthSd), ')']); 
-            disp(['HV: ', num2str(obj.FAhealthControlsResults.strengthMean), ' (', num2str(obj.FAhealthControlsResults.strengthSd), ')']); 
+            disp(['MS: ', num2str(obj.FApatientsResults.strengthMean), ' (', num2str(obj.FApatientsResults.strengthSd), ')']);
+            disp(['HV: ', num2str(obj.FAhealthControlsResults.strengthMean), ' (', num2str(obj.FAhealthControlsResults.strengthSd), ')']);
             disp(['p-value: ', num2str(obj.FAhealthControlsResults.strengthPvalue)]);
             
             disp('--------------------');
             disp('STRENGHT - SC MATRIX');
-            disp(['MS: ', num2str(obj.SCpatientsResults.strengthMean), ' (', num2str(obj.SCpatientsResults.strengthSd), ')']); 
-            disp(['HV: ', num2str(obj.SChealthControlsResults.strengthMean), ' (', num2str(obj.SChealthControlsResults.strengthSd), ')']); 
+            disp(['MS: ', num2str(obj.SCpatientsResults.strengthMean), ' (', num2str(obj.SCpatientsResults.strengthSd), ')']);
+            disp(['HV: ', num2str(obj.SChealthControlsResults.strengthMean), ' (', num2str(obj.SChealthControlsResults.strengthSd), ')']);
             disp(['p-value: ', num2str(obj.SChealthControlsResults.strengthPvalue)]);
+            
+        end
+        
+        function showPlots(obj)
+            strengthsLinePlot(obj, 'FAMatrix');
+            strengthsLinePlot(obj, 'SCMatrix');
+            strengthsBoxPlot(obj, 'FAMatrix');
+            strengthsBoxPlot(obj, 'SCMatrix');
+            
+        end
+        
+        % This function shows a line plot of the mean values of the
+        % strengths of each node for each population
+        %
+        % @param matrixType must be either 'FAMatrix' or 'SCMatrix'
+        function strengthsLinePlot(obj, matrixType)
+            % If a node has a strength higher than the average and 1
+            % standard deviation of its group of participants, it is
+            % considered a hub node. This value will be called hubLimit
+            hubLimitPatients = 0;
+            hubLimitHealthControls = 0;
+            
+            % First we need to combine all strength vectors into a matrix
+            if strcmp(matrixType, 'FAMatrix') == true
+                for i = 1:length(obj.patients)
+                    patientsStrengths(i,:) = obj.patients(i).FAMatrix.strengths;
+                end
+                for i = 1:length(obj.healthControls)
+                    healthControlsStrengths(i,:) = obj.healthControls(i).FAMatrix.strengths;
+                end
+                hubLimitPatients = obj.FApatientsResults.strengthMean + obj.FApatientsResults.strengthSd;
+                hubLimitHealthControls = obj.FAhealthControlsResults.strengthMean + obj.FAhealthControlsResults.strengthSd;
+                
+                plotTitle = 'Strength-FA Matrix';
+                
+            elseif strcmp(matrixType, 'SCMatrix')
+                for i = 1:length(obj.patients)
+                    patientsStrengths(i,:) = obj.patients(i).SCMatrix.strengths;
+                end
+                for i = 1:length(obj.healthControls)
+                    healthControlsStrengths(i,:) = obj.healthControls(i).SCMatrix.strengths;
+                end
+                hubLimitPatients = obj.SCpatientsResults.strengthMean + obj.SCpatientsResults.strengthSd;
+                hubLimitHealthControls = obj.SChealthControlsResults.strengthMean + obj.SChealthControlsResults.strengthSd;
+                
+                plotTitle = 'Strength-SC Matrix';                
+                
+            end
+            
+            % Then we obtain the mean value of each column of the strength
+            % matrices. This is the mean strength value of each brain node
+            % across each population
+            ms_mean_by_node = mean(patientsStrengths);
+            hv_mean_by_node = mean(healthControlsStrengths);
+            
+            % Now we plot the values
+            figure
+            plot(hv_mean_by_node, 'k-o', 'MarkerFaceColor', 'k', 'MarkerSize', 4,'LineWidth', 2);
+            title(plotTitle);
+            hold on;
+            plot(ms_mean_by_node, 'b-o', 'MarkerFaceColor', 'b', 'MarkerSize', 4, 'LineWidth', 2);
+            legend('hv', 'ms');
+            xlabel('Nodes');
+            
+            % Plot the hub limits
+            plot(hubLimitHealthControls*ones(80), 'k', 'LineWidth', 2);
+            plot(hubLimitPatients*ones(80), 'b', 'LineWidth', 2);
+ 
+            hold off
+        end
+        
+        % This function plots the boxplot of both groups
+        %
+        % @param matrixType must be either 'FAMatrix' or 'SCMatrix'
+        function strengthsBoxPlot(obj, matrixType)
+            % First we need to combine all strength vectors into a matrix
+            if strcmp(matrixType, 'FAMatrix') == true
+                for i = 1:length(obj.patients)
+                    patientsStrengths(i,:) = obj.patients(i).FAMatrix.strengths;
+                end
+                for i = 1:length(obj.healthControls)
+                    healthControlsStrengths(i,:) = obj.healthControls(i).FAMatrix.strengths;
+                end
+                
+                plotTitle = 'Strength-FA Matrix';
+                
+            elseif strcmp(matrixType, 'SCMatrix')
+                for i = 1:length(obj.patients)
+                    patientsStrengths(i,:) = obj.patients(i).SCMatrix.strengths;
+                end
+                for i = 1:length(obj.healthControls)
+                    healthControlsStrengths(i,:) = obj.healthControls(i).SCMatrix.strengths;
+                end
+                
+                plotTitle = 'Strength-SC Matrix';                
+                
+            end
+            
+            % Then we obtain the mean value of each column of the strength
+            % matrices. This is the mean strength value of each brain node
+            % across each population
+            ms_mean_by_node = mean(patientsStrengths);
+            hv_mean_by_node = mean(healthControlsStrengths);
+            
+            % Now we combine both groups of data into a matrix
+            X(:,1) = hv_mean_by_node;
+            X(:,2) = ms_mean_by_node;
+            
+            %Plot the values
+            figure
+            boxplot(X, 'Color', 'k', 'Labels',{'HV','MS'})
+            title(plotTitle)
+            
             
         end
         
