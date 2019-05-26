abf5d0b59ef58e1d7eddf58f42f73e919c4d8dcc
saraen <saraestravis@gmail.com>
2019-05-26 14:48:09 +0200

    Implement Main function

---
+++ Main.m
@@ -0,0 +1,11 @@
+clear;
+brainMatrixCohort = Cohort;
+brainMatrixCohort.loadCohortMembersFromFile('CONN_UOC\Dem_clin_MS_HV.csv');
+
+brainMatrixCohort.analizeCohort();
+brainMatrixCohort.evaluateCohort();
+brainMatrixCohort.showAnalysisResults();
+brainMatrixCohort.showPlots();
+
+featuresMatrix  = getFeaturesMatrix(brainMatrixCohort);
+saveDataToCsv(featuresMatrix, 0, 'pruebaCsv');
