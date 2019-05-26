8531d03384c5d3062492e7770ccdbce098e6b276
saraen <saraestravis@gmail.com>
2019-05-26 21:05:50 +0200

    Reordering of matlab files

--- matlab/Main.m
+++ matlab/Main.m
@@ -5,7 +5,7 @@ brainMatrixCohort.loadCohortMembersFromFile('CONN_UOC\Dem_clin_MS_HV.csv');
 brainMatrixCohort.analizeCohort();
 brainMatrixCohort.evaluateCohort();
 brainMatrixCohort.showAnalysisResults();
-brainMatrixCohort.showPlots();
+% brainMatrixCohort.showPlots();
 
 featuresMatrix  = getFeaturesMatrix(brainMatrixCohort);
 saveDataToCsv(featuresMatrix, 0, 'BrainMatrixData');
